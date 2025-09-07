f24aec91c6d29fc83d8600a6147c531632256c86
Wang Huy <daoquanghuy2907@gmail.com>
2025-09-07 10:48:11 +0700

    feat: add GTM tag for analysing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,7 @@ import Footer from "@/components/footer";
 import { DataSeo } from "@/libs/constants";
 import NextTopLoader from "nextjs-toploader";
 import ButtonGo2Top from "@/components/button-go-2-top";
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
@@ -41,6 +42,23 @@ export default function RootLayout({ children }) {
 
         <link rel="stylesheet" href="/assets/css/flaticon.css" />
         <link rel="stylesheet" href="/assets/css/style.css" />
+
+        {/* <!-- Google Tag Manager --> */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TFNTJFWW');
+          `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body
         className={`${interFont.className} antialiased text-[var(--text-color)]`}
@@ -51,6 +69,17 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </main>
+
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TFNTJFWW"
+            height="0"
+            width="0"
+            style="display:none; visibility:hidden"
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
       </body>
     </html>
   );
